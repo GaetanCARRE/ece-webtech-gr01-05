5bc17e5e7031a2a6ee0991afc3c80c8089536231
Gaëtan Carré <gaetancarre@MacBook-Pro-de-Gaetan.local>
2023-02-16 13:25:15 +0100

    feat : entire Lab1

--- handles.js
+++ handles.js
@@ -1,3 +1,20 @@
+const qs = require('querystring')
+const url = require('url')
+const fs = require('fs');
+
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' +
+'    <body>' +
+'    </body>' +
+'</html>'
+
+const home = '<h1>Home</h1>' +
+'<p>By typing /hello?name="your name" in url you can display your name\nif you dont write name it will say Hello Anonymous. Try it <a href="http://localhost:8080/hello">here</a> </p>'
+
 module.exports = {
     serverHandle : function (req, res) {
         const path = url.parse(req.url).pathname;
@@ -10,12 +27,27 @@ module.exports = {
     
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(content);
-    
-        if (route_path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-        } else {
+        if (route_path == '/') {
+            res.write(home)
+        }
+        else if(route_path === '/hello' && 'name' in params) {
+            if (params['name'] == 'Gaetan')
+                res.write('Hello i\'m Gaetan CARRE,\n i\'m 21 years old and i\'m a student at ECE Paris in apprentiship with Thales')
+            else
+                res.write('Hello ' + params['name'])
+        } 
+        else if(route_path === '/hello'){
             res.write('Hello anonymous')
         }
+        else if (route_path === '/about'){
+            const about = JSON.parse(fs.readFileSync('about.json'))
+            res.write(about.content)
+
+        }
+        else {
+            res.write('<h1>404 Not Found</h1>')
+        }
+        
         
         res.end();
       
--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-import {serverHandle} from './handles.js'
-
 const http = require('http')
-const qs = require('querystring')
-
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' +
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
 const url = require('url')
 const handles = require('./handles')
-const server = http.createServer(serverHandle)
+const server = http.createServer(handles.serverHandle);
 server.listen(8080)
