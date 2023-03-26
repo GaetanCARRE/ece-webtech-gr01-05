cb9aa3097f1d6a426b415f3e011841fc1658b3a5
GaetanCARRE <gaetan.carre74@gmail.com>
2023-03-26 18:13:58 +0200

    feat : home page UI improovement + carousel

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,41 +7,43 @@ const HeaderComponent = () => {
   // vérifier si un utilisateur est connecté
   const user = useUser()
   return (
-    <div className="bg-black text-white py-4 mb-10">
-      <div className="max-w-7xl mx-auto px-4 bg-transparent">
-        <div className="flex justify-between items-center bg-transparent">
-          <h1 className="text-2xl font-bold bg-transparent">CNS</h1>
-          <nav className='bg-transparent'>
-            <ul className="flex space-x-4 bg-transparent">
-              <li className='bg-transparent'>
-                <Link href="/" className="hover:text-gray-400 bg-transparent">
-                  <BiHomeAlt2 className='bg-transparent'/>
-                </Link>
-              </li>      
-              <li className='bg-transparent'>
-                <Link href="/articles" className="hover:text-gray-400 bg-transparent">
-                  Articles
-                </Link>
-              </li>
-              {/* si l'utilisateur est connecté, afficher le bouton de déconnexion, sinon afficher le bouton de connexion */}
-              {user ? (
+    <header>
+      <div className="bg-black text-white py-4" id='head'>
+        <div className="max-w-7xl mx-auto px-4 bg-transparent">
+          <div className="flex justify-between items-center bg-transparent">
+            <h1 className="text-2xl font-bold bg-transparent">CNS</h1>
+            <nav className='bg-transparent'>
+              <ul className="flex space-x-4 bg-transparent">
                 <li className='bg-transparent'>
-                  <button className="button block bg-transparent" onClick={() => supabase.auth.signOut()}>
-                    <BiLogOutCircle className='bg-transparent'/>
-                  </button>
-                </li>
-              ) : (
-                <li className='bg-transparent'>
-                  <Link href="/login" className="hover:text-gray-400 bg-transparent">
-                    <BiLogInCircle className='bg-transparent'/>
+                  <Link href="/" className="hover:text-gray-400 bg-transparent">
+                    <BiHomeAlt2 className='bg-transparent' />
                   </Link>
                 </li>
-              )}
-            </ul>
-          </nav>
+                <li className='bg-transparent'>
+                  <Link href="/articles" className="hover:text-gray-400 bg-transparent">
+                    Articles
+                  </Link>
+                </li>
+                {/* si l'utilisateur est connecté, afficher le bouton de déconnexion, sinon afficher le bouton de connexion */}
+                {user ? (
+                  <li className='bg-transparent'>
+                    <button className="button block bg-transparent" onClick={() => supabase.auth.signOut()}>
+                      <BiLogOutCircle className='bg-transparent' />
+                    </button>
+                  </li>
+                ) : (
+                  <li className='bg-transparent'>
+                    <Link href="/login" className="hover:text-gray-400 bg-transparent">
+                      <BiLogInCircle className='bg-transparent' />
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
