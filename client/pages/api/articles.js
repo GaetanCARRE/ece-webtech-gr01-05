5c752b079cd780b62454743964e643c7f7ed16da
GaetanCARRE <gaetan.carre74@gmail.com>
2023-04-05 21:41:59 +0200

    feat: use supabase to fetch data

--- client/pages/api/articles.js
+++ client/pages/api/articles.js
@@ -1,7 +1,11 @@
-import articlesData from '../../data/articles.json';
-export const db = articlesData
-  
-  export default function handler(req, res) {
-    res.status(200).json(db)
+import { supabase } from '../../supabase/supabase.js';
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase.from('articles').select('*');
+  if (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-  
+
+  return res.status(200).json(data);
+}
