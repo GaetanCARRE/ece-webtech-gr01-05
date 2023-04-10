9703b5fd2df50397ccbf4a34b1172301d17c2fc0
GaetanCARRE <gaetan.carre74@gmail.com>
2023-04-10 17:22:18 +0200

    style: contact page

--- client/pages/contacts.js
+++ client/pages/contacts.js
@@ -1,23 +1,23 @@
-// pages/contacts.js
-
 import Link from 'next/link';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function ContactsPage() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <div>
-        <h1>Contact Us</h1>
-        <ul>
-          <li>Email: <Link href="mailto:contact@myapp.com">gaetan.carre@edu.ece.fr</Link></li>
-          <li>Phone: <Link href="tel:0781762870">0781762870</Link></li>
-          <li>GitHub: <Link href="github.com/GaetanCARRE">GaetanCARRE</Link></li>
+      <div className='text-black text-center flex-grow'>
+        <h1 className='text-4xl p-10'>Contact Us</h1>
+        <ul className='text-xl'>
+          <li className='pb-2'>Email:</li>
+          <li className='pb-8 hover:text-gray-500'> <Link href="mailto:contact@myapp.com">gaetan.carre@edu.ece.fr</Link></li>
+          <li className='pb-2'>Phone:</li>
+          <li className='pb-8 hover:text-gray-500'> <Link href="tel:0781762870">0781762870</Link></li>
+          <li className='pb-2'>GitHub:</li>
+          <li className='hover:text-gray-500'> <Link href="https://github.com/GaetanCARRE">GaetanCARRE</Link></li>
         </ul>
-        <p>Click <Link href="/">here</Link> to go back to the homepage.</p>
       </div>
-      <Footer />
-    </>
+      <Footer className="flex-shrink-0" />
+    </div>
   );
 }
