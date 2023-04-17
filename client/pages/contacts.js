33b390599f6990f66262d775a397f68f3a2087f4
GaetanCARRE <gaetan.carre74@gmail.com>
2023-04-17 08:53:17 +0200

    feat: update contact page with supabase

--- client/pages/contacts.js
+++ client/pages/contacts.js
@@ -1,23 +1,152 @@
-import Link from 'next/link';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useState } from 'react';
+import { supabase } from '../supabase/supabase';
 
-export default function ContactsPage() {
+function Contact() {
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const { data, error } = await supabase.from('contacts').insert({
+        firstname,
+        lastname,
+        email,
+        message,
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('Contact submitted:', data);
+      alert('Thank you for your message!');
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setMessage('');
+    } catch (error) {
+      console.log(error);
+      alert(error.error_description || error.message);
+    }
+  }
+
   return (
-    <div className="flex flex-col h-screen">
+    <>
       <Header />
-      <div className='text-black text-center flex-grow'>
-        <h1 className='text-4xl p-10'>Contact Us</h1>
-        <ul className='text-xl'>
-          <li className='pb-2'>Email:</li>
-          <li className='pb-8 hover:text-gray-500'> <Link href="mailto:contact@myapp.com">gaetan.carre@edu.ece.fr</Link></li>
-          <li className='pb-2'>Phone:</li>
-          <li className='pb-8 hover:text-gray-500'> <Link href="tel:0781762870">0781762870</Link></li>
-          <li className='pb-2'>GitHub:</li>
-          <li className='hover:text-gray-500'> <Link href="https://github.com/GaetanCARRE">GaetanCARRE</Link></li>
-        </ul>
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div
+          className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Contact Us
+          </h2>
+        </div>
+        <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  value={firstname}
+                  onChange={(event) => setFirstName(event.target.value)}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400required"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  value={lastname}
+                  onChange={(event) => setLastName(event.target.value)}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="4"
+                  value={message}
+                  onChange={(event) => setMessage(event.target.value)}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  required
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div className="mt-8 text-center sm:mt-10">
+            <button
+              type="submit"
+              className="inline-block w-full max-w-xs font-medium rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:max-w-none sm:px-8"
+            >
+              Send message
+            </button>
+          </div>
+        </form>
       </div>
-      <Footer className="flex-shrink-0" />
-    </div>
+      <Footer />
+    </>
   );
 }
+
+export default Contact;
