15dc9b75b70e118e6358356ce612d51fdad26919
GaetanCARRE <gaetan.carre74@gmail.com>
2023-04-29 09:04:29 +0200

    fix: header and contact

--- client/components/Header.js
+++ client/components/Header.js
@@ -39,7 +39,7 @@ export default function Header() {
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Link href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <Image src={logo}
               alt="Picture of the author" className="h-12 w-auto" />
--- client/pages/contacts.js
+++ client/pages/contacts.js
@@ -73,7 +73,8 @@ function Contact() {
                   autoComplete="given-name"
                   value={firstname}
                   onChange={(event) => setFirstName(event.target.value)}
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400required"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
+                  required
                 />
               </div>
             </div>
